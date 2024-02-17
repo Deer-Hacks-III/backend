782014bc01ffc487890b6425d347b4fcd7b139d9
whomst <whomstvnted@gmail.com>
2024-02-17 16:59:33 -0500

    modified auth0 code

--- app.py
+++ app.py
@@ -1,127 +1,50 @@
-from authlib.integrations.flask_client import OAuth
-<<<<<<< HEAD
-from flask import Flask
+from authlib.integrations.flask_oauth2 import ResourceProtector
+from flask import Flask, url_for, session, redirect
 from flask import jsonify
-from os import environ as env
 from db import db
+from validator import Auth0JWTBearerTokenValidator
 import pymongo
 
 app = Flask(__name__)
 
-# auth0
-=======
-from flask import Flask, render_template, url_for, session, redirect, jsonify, request, _request_ctx_stack
-from functools import wraps
-from jose import jwt
-from os import environ as env
-from pymongo import MongoClient
-from six.moves.urllib.request import urlopen
-
-import json
-
-AUTH0_DOMAIN = '{yourDomain}'
-API_AUDIENCE = "https://bargain/api"
-ALGORITHMS=["RS256"]
-
-app = Flask(__name__)
-
-class AuthError(Exception):
-    def __init__(self, error, status_code):
-        self.error = error
-        self.status_code = status_code
-
-@app.errorhandler(AuthError)
-def handle_auth_error(ex):
-    response = jsonify(ex.error)
-    response.status_code = ex.status_code
-    return response
-
-def get_token_auth_header():
-    """Obtains the Access Token from the Authorization Header
-    """
-    auth = request.headers.get("Authorization", None)
-    if not auth:
-        raise AuthError({"code": "authorization_header_missing",
-                        "description":
-                            "Authorization header is expected"}, 401)
-
-    parts = auth.split()
-
-    if parts[0].lower() != "bearer":
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must start with"
-                            " Bearer"}, 401)
-    elif len(parts) == 1:
-        raise AuthError({"code": "invalid_header",
-                        "description": "Token not found"}, 401)
-    elif len(parts) > 2:
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must be"
-                            " Bearer token"}, 401)
-
-    token = parts[1]
-    return token
-
-def requires_auth(f):
-    """Determines if the Access Token is valid
-    """
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = get_token_auth_header()
-        jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
-        jwks = json.loads(jsonurl.read())
-        unverified_header = jwt.get_unverified_header(token)
-        rsa_key = {}
-        for key in jwks["keys"]:
-            if key["kid"] == unverified_header["kid"]:
-                rsa_key = {
-                    "kty": key["kty"],
-                    "kid": key["kid"],
-                    "use": key["use"],
-                    "n": key["n"],
-                    "e": key["e"]
-                }
-        if rsa_key:
-            try:
-                payload = jwt.decode(
-                    token,
-                    rsa_key,
-                    algorithms=ALGORITHMS,
-                    audience=API_AUDIENCE,
-                    issuer="https://"+AUTH0_DOMAIN+"/"
-                )
-            except jwt.ExpiredSignatureError:
-                raise AuthError({"code": "token_expired",
-                                "description": "token is expired"}, 401)
-            except jwt.JWTClaimsError:
-                raise AuthError({"code": "invalid_claims",
-                                "description":
-                                    "incorrect claims,"
-                                    "please check the audience and issuer"}, 401)
-            except Exception:
-                raise AuthError({"code": "invalid_header",
-                                "description":
-                                    "Unable to parse authentication"
-                                    " token."}, 401)
-
-            _request_ctx_stack.top.current_user = payload
-            return f(*args, **kwargs)
-        raise AuthError({"code": "invalid_header",
-                        "description": "Unable to find appropriate key"}, 401)
-    return decorated
-
-oauth = OAuth(app)
-oauth.register(
-    "auth0",
-    client_id=env.get("AUTH0_CLIENT_ID"),
-    client_secret=env.get("AUTH0_CLIENT_SECRET"),
-    client_kwargs={
-        "scope": "openid profile email",
-    },
-    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+require_auth = ResourceProtector()
+validator = Auth0JWTBearerTokenValidator(
+    "127.0.0.1:5000",
+    "https://bargain/api"
 )
+require_auth.register_token_validator(validator)
+
+@app.route("/api/public")
+def public():
+    """No access token required."""
+    response = (
+        "Hello from a public endpoint! You don't need to be"
+        " authenticated to see this."
+    )
+    return jsonify(message=response)
+
+
+@app.route("/api/private")
+@require_auth(None)
+def private():
+    """A valid access token is required."""
+    response = (
+        "Hello from a private endpoint! You need to be"
+        " authenticated to see this."
+    )
+    return jsonify(message=response)
+
+
+@app.route("/api/private-scoped")
+@require_auth("read:messages")
+def private_scoped():
+    """A valid access token and scope are required."""
+    response = (
+        "Hello from a private endpoint! You need to be"
+        " authenticated and have a scope of read:messages to see"
+        " this."
+    )
+    return jsonify(message=response)
 
 list = pymongo.collection.Collection(db, 'list')
 
