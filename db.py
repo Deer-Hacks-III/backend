415374e06d06e6eb2359befc940c4c8e68984d52
renee-k <nahleekhan@gmail.com>
2024-02-17 14:48:49 -0500

    started implementing datebase operations

--- db.py
+++ db.py
@@ -1,18 +1,38 @@
 from flask import Flask
 import pymongo
 from app import app
+
 CONNECTION_STRING = "mongodb+srv://mainuser:demopassword@workshop.buro7dq.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING, server_api=pymongo.server_api.ServerApi('1'))
-db = client.get_database('flask_mongodb_atlas')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
+db = client.get_database('grocery')
+list = pymongo.collection.Collection(db, 'list')
 
-@app.route("/upc/", methods=["POST"])
-def add_upc():
-    pass
+# app.config["MONGO_URI"] = "mongodb+srv://mainuser:demopassword@workshop.buro7dq.mongodb.net/?retryWrites=true&w=majority"
+# mongo = pymongo.PyMongo(app)
 
-@app.route("/upc/", methods=["GET"])
-def get_upc():
-    pass
+# @app.route("/upc/", methods=["POST"])
+def add_upc(upc):
+    if not get_upc(upc):
+        list.insert_one({'upc': upc})
+    else: # upc already in the database
+        pass
+
+
+# @app.route("/upc/", methods=["GET"])
+def get_upc(upc):
+    result = list.find_one()
+
+    if not result:
+        # return Flask.jsonify({'upc_exists': False}), 404
+        return False
+    else:
+        # return Flask.jsonify({'msg': True}), 200
+        return True
+
+
+def delete_upc(upc):
+    if get_upc(upc):
+        list.delete_one({'upc': upc})
 
 try:
     client.admin.command('ping')
