cc95e52116e26551fe83c25497c2afbd02d28411
renee-k <nahleekhan@gmail.com>
2024-02-17 16:33:05 -0500

    fixed flask api

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from authlib.integrations.flask_client import OAuth
 from flask import Flask
+from flask import jsonify
 from os import environ as env
+from db import db
+import pymongo
 
 app = Flask(__name__)
 
+# auth0
 oauth = OAuth(app)
 oauth.register(
     "auth0",
@@ -15,9 +19,44 @@ oauth.register(
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
 )
 
+list = pymongo.collection.Collection(db, 'list')
+
 @app.route("/")
 def home():
     return "hi"
+
+@app.route("/list/", methods=["POST"])
+def add_upc(upc):
+    # json.loads(request.data)
+    if not get_upc(upc):
+        list.insert_one({'upc': upc})
+        return jsonify({'upc_added': True}), 200
+    else: # upc already in the database
+        return jsonify({'upc_added': False}), 400
+
+
+@app.route("/list/<int:upc>", methods=["GET"])
+def get_upc(upc: int):
+    result = list.find_one({'upc': upc})
+
+    if result:
+        return jsonify({'upc_exists': True}), 200
+        # return False
+    else:
+        return jsonify({'upc_exists': False}), 404
+        # return True
+
+@app.route('/list/<int:upc>', methods=['DELETE'])
+def delete_upc(upc: int):
+    if get_upc(upc):
+        list.delete_one({'upc': upc})
+        return jsonify({'upc_deleted': True}), 200
+    else:
+        return jsonify({'upc_deleted': False}), 400
+
+@app.route('/list/', methods=['GET'])
+def get_all_upcs():
+    return jsonify([x for x in list.find({}, {"_id": 0, "upc": 1})]) # maybe add id too?
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- db.py
+++ db.py
@@ -1,47 +1,12 @@
 from flask import Flask
 import pymongo
-from app import app
 
 CONNECTION_STRING = "mongodb+srv://mainuser:demopassword@workshop.buro7dq.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING, server_api=pymongo.server_api.ServerApi('1'))
 db = client.get_database('grocery')
-list = pymongo.collection.Collection(db, 'list')
 
 # app.config["MONGO_URI"] = "mongodb+srv://mainuser:demopassword@workshop.buro7dq.mongodb.net/?retryWrites=true&w=majority"
 # mongo = pymongo.PyMongo(app)
-
-# @app.route("/list/", methods=["POST"])
-def add_upc(upc):
-    # json.loads(request.data)
-    if not get_upc(upc):
-        list.insert_one({'upc': upc})
-    else: # upc already in the database
-        print("adding not success")
-
-
-# @app.route("/list/<int:upc>", methods=["GET"])
-def get_upc(upc: int):
-    result = list.find_one({'upc': upc})
-
-    if not result:
-        # return Flask.jsonify({'upc_exists': False}), 404
-        return False
-    else:
-        # return Flask.jsonify({'upc_exists': True}), 200
-        return True
-
-# @app.route('/list/<int:upc>', methods=['DELETE'])
-def delete_upc(upc: int):
-    if get_upc(upc):
-        # return Flask.jsonify({'upc_deleted': True}), 200
-        list.delete_one({'upc': upc})
-        print("deleted")
-    else:
-        # return Flask.jsonify({'upc_deleted': False}), 400
-        print("not deleted")
-
-def get_all_upcs():
-    return Flask.jsonify([x for x in list.find({}, {"_id": 0, "upc": 1})]) # maybe add id too?
 
 try:
     client.admin.command('ping')
