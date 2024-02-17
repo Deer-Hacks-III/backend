d1b10dcb4b7bac11c118583df5f2bb9a7e5295e8
whomst <whomstvnted@gmail.com>
2024-02-17 13:56:29 -0500

    starter auth

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from authlib.integrations.flask_client import OAuth
 from flask import Flask
+from os import environ as env
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 
 oauth = OAuth(app)
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
+
+@app.route("/")
+def home():
+    return "hi"
